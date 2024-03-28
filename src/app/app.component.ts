a15c28de0a22a2c7a11aac23e689277f183d2a4e
Vinicius Zuppa <vcordeiro12@gmail.com>
2024-03-27 21:25:39 -0300

    Criado tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {NavComponent} from "./shared/nav/nav.component";
 import {FooterComponent} from "./shared/footer/footer.component";
 import {CardFilmeComponent} from "./components/card-filme/card-filme.component";
 import {ListFilmesComponent} from "./pages/user-area/list-filmes/list-filmes.component";
+import {LoginComponent} from "./pages/commons/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent, CardFilmeComponent, ListFilmesComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent, CardFilmeComponent, ListFilmesComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
